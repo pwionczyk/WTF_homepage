8a189b931ca958741f2a1f2dda84d2153cc43d78
Przemysław Wionczyk <przemyslaw@wionczyk.com>
2020-07-17 00:06:54 +0200

    update headers with HTML, CSS and JS logotype images & using border-box

--- scripts.js
+++ scripts.js
@@ -1,2 +1,5 @@
-name = "Przemysław Wionczyk";
+let name = "Przemysław Wionczyk";
 console.log(`Hi, my name is ${name}. I built this site.`);
+
+const owner = document.querySelector('.site-footer__owner--js');
+owner.innerHTML = 'Przemysława Wionczyka';
