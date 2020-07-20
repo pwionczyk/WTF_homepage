36f470f77b7c5dcc00114c7624567df82273c201
Przemysław Wionczyk <przemyslaw@wionczyk.com>
2020-07-20 17:37:59 +0200

    adding gradients and flow styles centering

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
 let name = "Przemysław Wionczyk";
 console.log(`Hi, my name is ${name}. I built this site.`);
 
-const owner = document.querySelector('.site-footer__owner--js');
-owner.innerHTML = 'Przemysława Wionczyka';
+const owner = document.querySelector(".site-footer__owner--js");
+owner.innerHTML = "Przemysława Wionczyka";
+
+const invitation2 = (name, age) => {
+  if ([2, 3, 4].includes(age % 10))
+    alert(`Dzień dobry. Nazywasz się ${name} i masz ${age} lata.`);
+  else alert(`Dzień dobry. Nazywasz się ${name} i masz ${age} lat.`);
+};
+
+function invitation(name, age) {
+  if ([2, 3, 4].includes(age % 10))
+    alert(`Dzień dobry. Nazywasz się ${name} i masz ${age} lata.`);
+  else alert(`Dzień dobry. Nazywasz się ${name} i masz ${age} lat.`);
+}
